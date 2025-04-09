351de384c3a1190e86790c3f4beda63e615b300c
Vladislav Davidov <davidovvd1@gmail.com>
2025-04-09 03:56:46 +0300

    THIRD COMMIT

---
+++ src/Hotel.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+
+public class Hotel {
+    List<Room> rooms = new ArrayList<>();
+    List<Reservation> reservations = new ArrayList<>();
+
+
+    public void addRoom(Room room) {
+        this.rooms.add(room);
+    }
+
+public void checkIn (int roomNumber, LocalDate from, LocalDate to, String note, int guests) {
+    for (Room room : rooms) {
+        if (roomNumber == room.getNumber() && !room.isOccupied()) {
+            reservations.add(new Reservation(roomNumber, from, to, note, guests));
+            room.setOccupied(true);
+            System.out.println("Room number " + roomNumber + " has been occupied.");
+            return;
+        }
+    }
+}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,43 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Hotel hotel = new Hotel();
+        Room room1 = new Room(120, 1, 3, false, false);
+        Room room2 = new Room(220, 2, 2, true, false);
+        Room room3 = new Room(320, 3, 4, true, false);
+        Room room4 = new Room(420, 4, 2, false, false);
+        hotel.addRoom(room1);
+        hotel.addRoom(room2);
+        hotel.addRoom(room3);
+        hotel.addRoom(room4);
+
+
         Scanner input = new Scanner(System.in);
-        String command = input.nextLine();
-        Menu menu = new Menu();
-        while (!command.equals("exit")) {
-            try {
-                menu.displayMenu(command);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        String command;
+        String file = "hotel.txt";
+        do {
+            command = input.nextLine();
+            switch (command) {
+                case "open":
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                        writer.write(" nova informacia");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.out.println("File couldn't be created");
+                    }
+                    System.out.println("File created");
+                    break;
+                case "exit":
+                    System.out.println("Exiting program...");
+                    break;
+                default:
+                    System.out.println("Invalid command");
             }
-        }
+        } while (!command.equals("exit"));
 
     }
 }
